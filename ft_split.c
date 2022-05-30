2a48699182c5bbf9fb51b6eaa480a4322f4ba5c0
Jules MUNTZ-BERGER <julmuntz@e2r5p8.clusters.42paris.fr>
2022-05-30 15:28:47 +0200

    ft_split | ft_strtrim | ft_substr, Unprotected.

--- ft_split.c
+++ ft_split.c
@@ -11,35 +11,34 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
-static int	ft_char_count(char *s, char c, int *i)
+static int	ft_char_count(char *s, char c, int *index_chars)
 {
-	int len;
+	int	len;
 
 	len = 0;
-	printf("Begins at s[%d] %c\n", *i, c);
-	while (s[*i] && s[*i] == c)
-		s++;
-	while (s[*i] && s[*i] != c)
-	{
-		(*i)++;
+	while (s[*index_chars] && s[*index_chars] == c)
+		(*index_chars)++;
+	while (s[*index_chars + len] && s[*index_chars + len] != c)
 		len++;
-	}
 	return (len);
 }
 
 static int	ft_word_count(char *s, char c)
 {
-	int i;
-	int count;
+	int	i;
+	int	count;
 
 	i = 0;
 	count = 0;
 	while (s[i])
 	{
-		i++;
-		if (s[i] != c)
+		if (s[i] == c)
+		{
+			while (s[i] && s[i] == c)
+				i++;
+		}
+		else
 		{
 			while (s[i] && s[i] != c)
 				i++;
@@ -49,70 +48,43 @@ static int	ft_word_count(char *s, char c)
 	return (count);
 }
 
-// char	ft_delim(char *s, char c)
-// {
-
-// }
-
-// static char	*ft_strtok_r(char *s, const char c, char **end)
-// {
-// 	char	*res;
-
-// 	if (s == NULL)
-// 		s = *end;
-// 	while (*s && ft_strchr(&c, *s))
-// 		++s;
-// 	if (*s == '\0')
-// 		return (NULL);
-// 	res = s;
-// 	while (*s && !ft_strchr(&c, *s))
-// 		++s;
-// 	if (*s)
-// 		*s++ = '\0';
-// 	*end = s;
-// 	return (res);
-// }
-
-// static char	*ft_strtok(char *s, const char c)
-// {
-// 	static char	*last;
-
-// 	return (ft_strtok_r(s, c, &last));
-// }
-
 char	**ft_split(char *s, char const c)
 {
-	int 	index_words;
-	int 	index_chars;
-	int		nb_words;
-	int 	nb_chars;
 	char	**tab;
+	int		index_words;
+	int		index_chars;
+	int		nb_words;
+	int		nb_chars;
 
-	nb_words = ft_word_count(s, c);
-	tab = ft_calloc(sizeof(char **), (nb_words));
+	if (s == NULL)
+		return (NULL);
 	index_words = 0;
 	index_chars = 0;
-	// printf("nb_words =  %d\n",nb_words);
+	nb_words = ft_word_count(s, c);
+	tab = ft_calloc(sizeof(char *), (nb_words + 1));
 	while (index_words < nb_words)
 	{
 		nb_chars = ft_char_count(s, c, &index_chars);
-		printf("%d/%d\n", index_chars, nb_chars);
 		tab[index_words] = ft_substr(s, index_chars, nb_chars);
+		index_chars += nb_chars;
 		index_words++;
 	}
 	return (tab);
 }
 
-///*
+/*
 
-int main()
+#include <stdio.h>
+int main(void)
 {
     int		i;
 	char	**ptr;
-	char	*str = "----ghsaefr-dfvf---fd--ghffxdd";
+	char	*str = "---abc-def--ghi----jkl-";
     char	chr = '-';
 	i = 0;
 	ptr = ft_split(str, chr);
+	if (ptr == NULL)
+		return (1);
 	while (ptr[i] != NULL)
 	{
 		printf("%s\n", ptr[i]);
@@ -123,4 +95,4 @@ int main()
 	return 0;
 }
 
-//*/
+*/
