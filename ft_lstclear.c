783c7a66ecfc553d3ef396faeefce385ad319569
Jules MUNTZ-BERGER <julmuntz@e2r8p19.clusters.42paris.fr>
2022-06-05 15:42:09 +0200

    ft_lstdelone & ft_lstclear, Done.

---
+++ ft_lstclear.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julmuntz <julmuntz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/05 14:46:54 by julmuntz          #+#    #+#             */
+/*   Updated: 2022/06/05 15:34:30 by julmuntz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*tmp;
+
+	while (*lst)
+	{
+		tmp = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = tmp;
+	}
+	*lst = NULL;
+}
